cbfdc3c2a9a94cf9a79e6ac15b132170aa67a8db
Scott Ouellette <scottx611x@gmail.com>
2017-11-15 15:17:59 -0500

    subprocess.check_call expects a list

--- ci_tests.py
+++ ci_tests.py
@@ -16,12 +16,19 @@ class ToolLoadingTests(unittest.TestCase):
     def test_tool_definition_generation(self):
         for tool_annotation_name in self.tool_annotation_names:
             print "Tool Annotation: ", tool_annotation_name
-            subprocess.check_call(
-                "python refinery-platform/refinery/manage.py load_tools --visualizations {}".format(
-                    tool_annotation_name
+            try:
+                subprocess.check_call(
+                    [
+                        "python",
+                        "refinery-platform/refinery/manage.py",
+                        "load_tools",
+                        "--visualizations",
+                        "{}".format(tool_annotation_name)
+                    ]
                 )
-            )
-
+            except subprocess.CalledProcessError as e:
+                print e
+                self.fail()
 
 if __name__ == "__main__":
     unittest.main()
