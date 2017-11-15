c6f399cbeb6d9a83eb418c826ea9b4f90702ba68
Scott Ouellette <scottx611x@gmail.com>
2017-11-15 15:09:39 -0500

    Master build works now, try to fix develop

--- ci_tests.py
+++ ci_tests.py
@@ -15,16 +15,13 @@ class ToolLoadingTests(unittest.TestCase):
 
     def test_tool_definition_generation(self):
         for tool_annotation_name in self.tool_annotation_names:
-            print tool_annotation_name
-            try:
-                subprocess.check_call(
-                    "python refinery-platform/refinery/manage.py load_tools --visualizations {}".format(
-                        tool_annotation_name
-                    )
+            print "Tool Annotation: ", tool_annotation_name
+            subprocess.check_call(
+                "python refinery-platform/refinery/manage.py load_tools --visualizations {}".format(
+                    tool_annotation_name
                 )
-            except subprocess.CalledProcessError as e:
-                print e
-                self.fail()
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
